6107a684f72c7744b6d970f9aa26fce034a120ee
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-09-29 20:18:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let photo = document.querySelector("#photo")
+var button = document.querySelector("#button")
+function change() {
+    if (button.innerHTML == "Изменить") {
+        //photo.src="cabbage.png"
+        button.innerHTML = "Вернуть"
+    }
+    else {
+        photo.src="left.png"
+        button.innerHTML = "Изменить"
+    }
+}
